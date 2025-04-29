984276e64f9735af9c397312b2db1a112e19d8c9
Karolin <kb.app.solutions@gmail.com>
2025-04-29 15:08:22 +0200

    Color scheme

--- app/src/main/java/com/awtech/cv_page/WelcomePage.kt
+++ app/src/main/java/com/awtech/cv_page/WelcomePage.kt
@@ -1,13 +1,69 @@
 package com.awtech.cv_page
 
-import androidx.compose.runtime.Composable
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 
 
 
 @Composable
+fun WelcomePage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to MyCV!",
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign = TextAlign.Center
+        )
 
-fun WelcomePage()
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Create your own digital business card.",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = "1.Click on the menu and choose Edit Profile",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = "2. Fill in you info and save.",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = "3. Your profile is created and ready to share.",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
 
 
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomePagePreview() {
+    WelcomePage()
 }
